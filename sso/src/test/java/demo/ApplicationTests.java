cf6189d850773bc07716575d426f17f3fc222484
sunlijiang <sunlijiang@mokahr.com>
2020-08-17 23:46:24 +0800

    update

--- sso/src/test/java/demo/ApplicationTests.java
+++ sso/src/test/java/demo/ApplicationTests.java
@@ -6,19 +6,14 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SsoApplication.class)
 @WebAppConfiguration
-@IntegrationTest({"debug", "server.port:0"})
 public class ApplicationTests {
 
 	@Value("${local.server.port}")
@@ -27,7 +22,7 @@ public class ApplicationTests {
 	@Value("${security.oauth2.client.userAuthorizationUri}")
 	private String authorizeUri;
 
-	private RestTemplate template = new TestRestTemplate();
+	private TestRestTemplate template = new TestRestTemplate();
 
 	@Test
 	public void homePageLoads() {
